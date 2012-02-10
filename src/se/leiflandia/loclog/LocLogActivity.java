8128838a7671ff43c1e9f21af358b5076da9e0db
Kristofer Leifland <peoplesbeer@gmail.com>
2012-02-10 20:21:18 +0100

    Settings now sent to Service + cleanup.

--- src/se/leiflandia/loclog/LocLogActivity.java
+++ src/se/leiflandia/loclog/LocLogActivity.java
@@ -1,36 +1,66 @@
 package se.leiflandia.loclog;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Toast;
+import android.widget.EditText;
 import android.widget.ToggleButton;
 
 public class LocLogActivity extends Activity {
 	private static final String TAG = LocLogActivity.class.getSimpleName();
-    /** Called when the activity is first created. */
+	private Intent logLocServiceIntent;
+	
+	private EditText edtMinTime;
+	private EditText edtMinDistance;
+	ToggleButton onOff;
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
-        final Intent intent = LocLogService.genStartIntent(getApplicationContext(), 5000, 1);
-        
-        final ToggleButton togglebutton = (ToggleButton) findViewById(R.id.tglLocLogService);
-        togglebutton.setOnClickListener(new OnClickListener() {
-        	public void onClick(View v) {
-        		if (togglebutton.isChecked()) {
-        			Log.d(TAG, "About to start service.");
-        			startService(intent);
-        		} else {
-        			Log.d(TAG, "About to stop service.");
-        			stopService(intent);
-        		}
-        	}
-        });
+
+        init();
+        onOff.setOnClickListener(new OnOffListener());
+    }
+    
+    private void init() {
+        logLocServiceIntent = LocLogService.genStartIntent(getApplicationContext());
+        edtMinTime = (EditText) findViewById(R.id.minTime);
+        edtMinDistance = (EditText) findViewById(R.id.minDistance);
+        onOff = (ToggleButton) findViewById(R.id.tglLocLogService);
+    }
+    
+    private void updateIntent() {
+    	long minTime = -1;
+    	try {
+    		minTime = Long.parseLong(edtMinTime.getText().toString());
+    	} catch (NumberFormatException e) {	}
+    	if (minTime >= 0)
+    		logLocServiceIntent.getExtras().putLong(LocLogService.KEY_MIN_TIME, minTime);
+    	
+    	float minDistance = -1f;
+    	try {
+    		minDistance = Float.parseFloat(edtMinDistance.getText().toString());
+    	} catch (NumberFormatException e) {	}
+    	if (minDistance >= 0f)
+    		logLocServiceIntent.getExtras().putFloat(LocLogService.KEY_MIN_DISTANCE, minDistance);
+    }
+    
+    private class OnOffListener implements OnClickListener {
+    	
+    	@Override
+    	public void onClick(View v) {
+    		if (onOff.isChecked()) {
+    			Log.d(TAG, "About to start service.");
+    			updateIntent();
+    			startService(logLocServiceIntent);
+    		} else {
+    			Log.d(TAG, "About to stop service.");
+    			stopService(logLocServiceIntent);
+    		}
+    	}
     }
 }
